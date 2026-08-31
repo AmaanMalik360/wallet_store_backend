08e38b2a3f2586b04d74f2a30860ab8145ccd417
AmaanMalik360 <amaanmalik0360@gmail.com>
2026-08-31 13:22:16 +0500

    currency migration correction

--- alembic/versions/c1d2e3f4a5b6_add_currencies_table.py
+++ alembic/versions/c1d2e3f4a5b6_add_currencies_table.py
@@ -13,7 +13,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = 'c1d2e3f4a5b6'
-down_revision: Union[str, Sequence[str], None] = 'e16bc5fb6fb2'
+down_revision: Union[str, Sequence[str], None] = '0971be3e9ab9'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -24,6 +24,13 @@ def upgrade() -> None:
         sa.Column('code', sa.CHAR(3), primary_key=True),
         sa.Column('decimal_places', sa.SmallInteger(), nullable=False, server_default='2'),
         sa.Column('is_default', sa.Boolean(), nullable=False, server_default='false'),
+    )
+    
+    # Insert PKR row so migration #3 can add FK constraint.
+    # NOTE: CurrencySeeder can be run after all migrations to ensure this row exists.
+    op.execute(
+        "INSERT INTO currencies (code, decimal_places, is_default) "
+        "VALUES ('PKR', 2, true)"
     )
 
 
